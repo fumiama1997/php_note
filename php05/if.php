fd4dc68018240181b117923e51706335f298ebd6
fumiama1997 <shiyetianye9@gmail.com>
2024-01-09 14:57:12 +0900

    1/9分

--- php05/if.php
+++ php05/if.php
@@ -2,7 +2,9 @@
 <?php
  
 $score = mt_rand(0, 100); //0以上100以下の数値をランダムで返す
- 
+ print $score . "\n";
+ var_dump($score >= 80);
+ var_dump($score <= 30);
 if ($score >= 80) {
     print 'ヽ( ﾟ∀ﾟ)ノｷﾀ━!!';
 } else if ($score <= 30) {
