e4b660074ce683e852f8f4cf2e68320b4e4305d7
fumiama1997 <shiyetianye9@gmail.com>
2024-02-07 08:41:47 +0900

    2/7

--- php09/practice_file_intermediate.php
+++ php09/practice_file_intermediate.php
@@ -1,13 +1,13 @@
 <!-- 日本郵便のページにある郵便番号のデータを利用し、「郵便番号」「住所」のデータを表示してください。 -->
 
-<?php 
+<?php
 $filename = './zip_data_split_1.txt';
-   
-if(is_readable($filename) == TRUE){
-    
-    if(($fp = fopen($filename,'r')) == TRUE){
-        while(($tmp = fgets($fp)) == TRUE){
-            $data[] = htmlspecialchars($tmp,ENT_QUOTES, 'UTF-8');
+
+if (is_readable($filename) == TRUE) {
+
+    if (($fp = fopen($filename, 'r')) == TRUE) {
+        while (($tmp = fgets($fp)) == TRUE) {
+            $data[] = htmlspecialchars($tmp, ENT_QUOTES, 'UTF-8');
         }
         fclose($fp);
     }
@@ -19,17 +19,20 @@ if(is_readable($filename) == TRUE){
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>郵便番号と住所</title>
 </head>
+
 <body>
-    <?php foreach($data as $address){
+    <?php foreach ($data as $address) {
         print $address . '<br>';
     } ?>
 
 
 
 </body>
+
 </html>
