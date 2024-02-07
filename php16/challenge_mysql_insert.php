3bf8a29040a8c1cdb101d2c607109b6042f5d3bf
fumiama1997 <shiyetianye9@gmail.com>
2024-02-07 15:01:59 +0900

    2/7分

--- php16/challenge_mysql_insert.php
+++ php16/challenge_mysql_insert.php
@@ -4,20 +4,29 @@ $error = [];
 $name = '';
 $price = '';
 
+$host = 'localhost'; // データベースのホスト名又はIPアドレス
+$username = 'root';  // MySQLのユーザ名
+$passwd   = 'narait';    // MySQLのパスワード
+$dbname   = 'user';    // データベース名
+$link = mysqli_connect($host, $username, $passwd, $dbname);
+
+// 接続成功した場合
+if ($link) {
+    // 文字化け防止
+    mysqli_set_charset($link, 'utf8');
+}
+
+$query = 'SELECT goods_name,price FROM goods_table';
+$result = mysqli_query($link, $query);
+
+while ($row = mysqli_fetch_array($result)) {
+    $goods_alldata[] = $row;
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $price = $_POST['price'];
-    $host = 'localhost'; // データベースのホスト名又はIPアドレス
-    $username = 'root';  // MySQLのユーザ名
-    $passwd   = 'narait';    // MySQLのパスワード
-    $dbname   = 'user';    // データベース名
-    $link = mysqli_connect($host, $username, $passwd, $dbname);
 
-    // 接続成功した場合
-    if ($link) {
-        // 文字化け防止
-        mysqli_set_charset($link, 'utf8');
-    }
     if ($name === '') {
         $error['name'] = '商品名を入力してください';
     }
@@ -29,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (empty($error)) {
         //ここの書き方が分からない
-        $query =  'INSERT INTO goods_table (goods_name, price) VALUES ("'.$name.'",'.$price.')';
+        $query =  'INSERT INTO goods_table (goods_name, price) VALUES ("' . $name . '",' . $price . ')';
 
         //追加 クエリを実行します  
         $result = mysqli_query($link, $query);
@@ -76,25 +85,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
 
     <form method="POST">
-        <h2>追加したい商品と価格を入力してください</h2>
-        <p>商品名: <input type="text" name="name"> 価格:<input type="text" name="price"><input type="submit" value="追加"></p>
+        <p>追加したい商品と価格を入力してください</p>
+        <p>商品名: <input type="text" name="name"> 価格:<input type="text" name="price"> <input type="submit" value="追加"></p>
     </form>
 
-    <h3>商品一覧</h3>
+    <p>商品一覧</p>
     <table>
         <tr>
             <th>商品名</th>
             <th>価格</th>
         </tr>
 
-
-        <!-- 追加ボタンを押す前のデータベースの情報を表示したい。 -->
-
-
-        <!-- エラーが出ていた場合 -->
-        <?php foreach ($error as $value) { ?>
-            <p><?php print $value; ?></p>
-        <?php } ?>
+        <!-- 追加ボタンを押す前のデータベースの情報 -->
+        <?php
+        if (empty($_POST)) {
+            foreach ($goods_alldata as $value) {
+        ?>
+                <tr>
+                    <td><?php print htmlspecialchars($value['goods_name'], ENT_QUOTES, 'UTF-8'); ?></td>
+                    <td><?php print htmlspecialchars($value['price'], ENT_QUOTES, 'UTF-8'); ?></td>
+                </tr>
+        <?php
+            }
+        }
+        ?>
         <!-- データベースに追加後のテーブル情報↓ -->
         <?php
         foreach ($goods_data as $value) {
@@ -106,6 +120,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <?php
         }
         ?>
+
+        <!-- エラーが出ていた場合 -->
+        <?php foreach ($error as $value) { ?>
+            <p><?php print $value; ?></p>
+        <?php } ?>
+
     </table>
 </body>
 
